0d4db9152f6312dc55d3b7f33e625e26ec79fd38
zack <2930777518@qq.com>
2023-06-26 20:04:18 +0800

    完善了segment接口

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ from PIL import Image
 from torchvision import transforms
 from hubs.model import MattingNetwork
 
-from models.common import UploadResult
+from models.common import UploadResult, SegmentResult
 
 app = Flask(__name__)
 
-STATIC_PATH = "http://39.100.68.34:8000" + "/static"
+STATIC_URL = "http://39.100.68.34:8000" + "/static"
+STATIC_PATH = "/static"
+SEGMENT_PATH = os.path.join(STATIC_PATH, 'segments')
 
 # 加载模型
 segmentor = MattingNetwork('mobilenetv3').eval()
@@ -36,18 +38,30 @@ def upload_image():
         return json.dumps(result)
 
 
-@app.route('/api/segment')
+@app.route('/api/segment', methods=['POST'])
 def segment():
-    # 读取图片，转换成Tensor
-    src = Image.open('../static/segments/001/e716048bd4ede0ab02440ec1f28f00c3.jpeg')
-    src = (transforms.PILToTensor()(src) / 255.)[None]
-    # 抠图
-    with torch.no_grad():
-        fgr, pha, *rec = segmentor(src)
-        segmented = torch.cat([src, pha], dim=1).squeeze(0).permute(1, 2, 0).numpy()
-        segmented = cv2.normalize(segmented, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-        Image.fromarray(segmented).show()
-    return 'Hello'
+    if request.method != 'POST':
+        result = SegmentResult('failed', '不支持的请求方式', None)
+        return json.dumps(result)
+    else:
+        # 获取上传的图片
+        f = request.files['file']
+        fpath = os.path.join(SEGMENT_PATH, f.filename)
+        f.save(fpath)
+        # 读取图片，转换成Tensor
+        src = Image.open(fpath)
+        src = (transforms.PILToTensor()(src) / 255.)[None]
+        # 抠图
+        with torch.no_grad():
+            fgr, pha, *rec = segmentor(src)
+            segmented = torch.cat([src, pha], dim=1).squeeze(0).permute(1, 2, 0).numpy()
+            segmented = cv2.normalize(segmented, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+            # 保存路径
+            save_name = "segment_" + f.filename.split(".")[0] + ".png"
+            save_path = os.path.join(SEGMENT_PATH, save_name)
+            Image.fromarray(segmented).save(save_path)
+        result = SegmentResult('success', '抠图成功', os.path.join(STATIC_URL, save_name))
+        return json.dumps(result)
 
 
 if __name__ == '__main__':
